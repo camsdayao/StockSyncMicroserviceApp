4d9ae94f670f430d6e79a706ffd88a29ff932f00
camsdayao <108874797+camsdayao@users.noreply.github.com>
2025-10-02 17:39:33 +0800

    Initial commit

---
+++ src/main/java/com/example/StockSyncApp/StockSyncAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockSyncApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSyncAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSyncAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockSyncApp/StockSyncAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockSyncApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
